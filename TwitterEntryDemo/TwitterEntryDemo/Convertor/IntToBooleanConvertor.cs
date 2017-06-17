a3a1d726ab1306a293d39596fa64fd9dbaeade38
Hozefa Mangalorewala <hozefam@gmail.com>
2017-06-17 14:38:02 +0300

    Add project files.

---
+++ TwitterEntryDemo/TwitterEntryDemo/Convertor/IntToBooleanConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TwitterEntryDemo.Convertor
+{
+    class IntToBooleanConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int minimumLength = System.Convert.ToInt32(parameter); //140
+            return ((int) value <= minimumLength);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TwitterEntryDemo/TwitterEntryDemo/Convertor/IntToColorConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TwitterEntryDemo.Convertor
+{
+    class IntToColorConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int minimumLength = System.Convert.ToInt32(parameter); //140
+            return ((int) value <= minimumLength) ? Color.Green : Color.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
